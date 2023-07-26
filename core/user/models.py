21e1dac545088b7ef6b242c166d01ce0237c470b
Viktor <viktorbezai@gmail.com>
2023-07-26 15:50:14 -0400

    User serializer + viewset

--- core/user/models.py
+++ core/user/models.py
@@ -5,6 +5,11 @@ from django.db import models
 from django.http import Http404
 
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return "user_{0}/{1}".format(instance.public_id, filename)
+
+
 class UserManager(BaseUserManager):
     def get_object_by_public_id(self, public_id):
         try:
@@ -52,17 +57,22 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     public_id = models.UUIDField(db_index=True, unique=True,
                                  default=uuid.uuid4, editable=False)
-    username = models.CharField(db_index=True,
-                                max_length=255, unique=True)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
     email = models.EmailField(db_index=True, unique=True)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
+
+    bio = models.TextField(null=True)
+    avatar = models.ImageField(null=True, blank=True, upload_to=user_directory_path)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now_add=True)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
     objects = UserManager()
 
     def __str__(self):
