9eaf5e866fd51cddd38e68c319bd9b94437ebc95
Viktor <viktorbezai@gmail.com>
2023-07-26 17:53:57 -0400

    add permissions

--- core/post/serializers.py
+++ core/post/serializers.py
@@ -3,6 +3,7 @@ from rest_framework.exceptions import ValidationError
 from core.abstract.serializers import AbstractSerializer
 from core.post.models import Post
 from core.user.models import User
+from core.user.serializers import UserSerializer
 
 
 class PostSerializer(AbstractSerializer):
@@ -12,6 +13,18 @@ class PostSerializer(AbstractSerializer):
         if self.context["request"].user != value:
             raise ValidationError("You can't create a post for another user.")
         return value
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        author = User.objects.get_object_by_public_id(rep["author"])
+        rep["author"] = UserSerializer(author).data
+        return rep
+
+    def update(self, instance, validated_data):
+        if not instance.edited:
+            validated_data['edited'] = True
+        instance = super().update(instance, validated_data)
+        return instance
 
     class Meta:
         model = Post
--- core/post/viewsets.py
+++ core/post/viewsets.py
@@ -3,13 +3,14 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from core.abstract.viewsets import AbstractViewSet
+from core.auth.permissions import UserPermission
 from core.post.models import Post
 from core.post.serializers import PostSerializer
 
 
 class PostViewSet(AbstractViewSet):
-    http_method_names = ('post', 'get')
-    permission_classes = (IsAuthenticated,)
+    http_method_names = ('post', 'get', 'put', 'delete')
+    permission_classes = (UserPermission,)
     serializer_class = PostSerializer
 
     def get_queryset(self):
