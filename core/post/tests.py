35810c8d0439bd0d03155a0d9b5451e97fbbc8d7
Viktor <viktorbezai@gmail.com>
2023-07-26 20:51:48 -0400

    Made tests

--- core/post/tests.py
+++ core/post/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+import pytest
+from core.fixtures.user import user
+from core.post.models import Post
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_create_post(user):
+    post = Post.objects.create(author=user,
+                               body="Test Post Body")
+    assert post.body == "Test Post Body"
+    assert post.author == user
